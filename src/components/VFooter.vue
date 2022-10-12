b2f57d5b4bc967b9d63b4d68c2ae50b19613cfaa
Joaquin (Pato) Decima <jdecima@teco.com.ar>
2022-10-12 20:41:11 -0300

    Add Footer

--- src/components/VFooter.vue
+++ src/components/VFooter.vue
@@ -1,7 +1,80 @@
 <template>
-    <footer>
-        
+    <footer id="footer">
+        <div class="footer-newsletter">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-6">
+                        <h4>Contacta con nosotros</h4>
+                        <p>Contactanos para poder resolver todas tus dudas y comenzar hoy con tu
+                            proyecto</p><a class="btn btn-contact"
+                            href="https://wa.me/541123882753?text=Hola,%20me%20gustaria%20saber%20mas%20sobre%20los%20productos%20y%20ofertas%20de%20Lynx"><em
+                                class="fa fa-whatsapp"></em></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4 col-md-6 footer-contact">
+                        <h3 class="v-font"><span>Vasak</span> group</h3>
+                        <p> GBA Sur (Quilmes - Berazategui) <br> Buenos Aires<br> Argentina
+                            <br><br><strong>Telefono:</strong> +54 9 11 2388-2753<br><strong>Email:</strong>
+                            info@vasak.net.ar<br></p>
+                    </div>
+                    <div class="col-lg-4 col-md-6 footer-links text-center">
+                        <h4>Nuestros Servicios Gratuitos</h4>
+                        <ul>
+                            <li>
+                                <em class="fa fa-chevron-right"></em>
+                                <a target="_black" href="https://qrgenerator.vasak.net.ar/">QR
+                                    Generator</a>
+                            </li>
+                            <li>
+                                <em class="fa fa-chevron-right"></em>
+                                <a target="_black"
+                                    href="https://play.google.com/store/apps/details?id=com.lynxqrcam">Lynx QRCam</a>
+                            </li>
+                            <li>
+                                <em class="fa fa-chevron-right"></em>
+                                <a target="_black" href="https://wppgenerator.vasak.net.ar/">WhatsApp
+                                    Generator</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-4 col-md-12 footer-links text-center">
+                        <h4>Nuestras Redes</h4>
+                        <p>No te pierdas todas nuestras novedades</p>
+                        <div class="social-links mt-3">
+                            <a target="_black" title="facebook" href="https://web.facebook.com/vasakgroup/">
+                                <em class="fa fa-facebook-square"></em>
+                            </a>
+                            <a target="_black" title="mail" href="mailto:info@vasak.net.ar">
+                                <em class="fa fa-envelope"></em>
+                            </a>
+                            <a target="_black" title="whatsapp"
+                                href="https://wa.me/541123882753?text=Hola,%20me%20gustaria%20saber%20mas%20sobre%20los%20productos%20y%20ofertas%20de%20Lynx">
+                                <em class="fa fa-whatsapp"></em>
+                            </a>
+                            <a target="_black" title="linkedin" href="https://www.linkedin.com/company/vasakgroup/">
+                                <em class="fa fa-linkedin"></em>
+                            </a>
+                            <a target="_black" title="instagram" href="https://www.instagram.com/vasakgroup/">
+                                <em class="fa fa-instagram"></em>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container py-4">
+            <div class="copyright"> © <span class="v-font">Vasak group</span>.
+                Todos los dertechos reservados. </div>
+            <div class="credits"><span class="v-font">Vasak</span></div>
+        </div>
     </footer>
 </template>
 <script></script>
-<style></style>
+<style>
+
+</style>
