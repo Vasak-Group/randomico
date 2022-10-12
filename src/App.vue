5f7aa13a20021452e9c754971eb403c8e0e8e28f
Joaquin (Pato) Decima <jdecima@teco.com.ar>
2022-10-12 20:58:28 -0300

    Add NavBar

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,14 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 import VFooter from './components/VFooter.vue'
+import NavBar from './components/NavBar.vue';
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+  <NavBar />
 
   <RouterView />
+
   <VFooter />
 </template>
 
---
+++ src/components/NavBar.vue
@@ -0,0 +1,28 @@
+<template>
+    <header id="header" class="d-flex align-items-center">
+        <div class="container d-flex align-items-center justify-content-between">
+            <a href="/" class="logo">
+                <img src="/img/logo.svg" alt="Vasak Group" title="Vasak Group">
+            </a>
+
+            <nav id="navbar" class="navbar">
+                <ul>
+                    <li>
+                        <RouterLink class="router-link-active router-link-exact-active nav-link" to="/">Home</RouterLink>
+                    </li>
+                </ul>
+                <em class="fa fa-list mobile-nav-toggle"></em>
+            </nav>
+        </div>
+    </header>
+</template>
+
+<script>
+import { RouterLink } from 'vue-router'
+
+export default {
+    components: {
+        RouterLink
+    }
+}
+</script>
