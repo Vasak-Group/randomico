d7346bbd08289dc8c31e8ff197e8b3e7eaf4499b
Joaquin (Pato) Decima <jdecima@teco.com.ar>
2022-11-10 11:10:08 -0300

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -15,5 +15,5 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Vasak-Group/randomico.git master:gh-pages
+git push -f git@github.com:Vasak-Group/randomico.git main:gh-pages
 cd -
