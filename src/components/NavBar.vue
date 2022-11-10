cb78b79c02ba138c9fd0f9f712cc14ee6d40e1e7
Joaquin (Pato) Decima <jdecima@teco.com.ar>
2022-11-10 10:34:59 -0300

    Cambio de logo

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,7 +2,7 @@
     <header id="header" class="d-flex align-items-center">
         <div class="container d-flex align-items-center justify-content-between">
             <a href="/" class="logo">
-                <img src="/img/logo.svg" alt="Vasak Group" title="Vasak Group">
+                <img src="/img/logo.png" alt="Vasak Group" title="Vasak Group">
             </a>
 
             <nav id="navbar" class="navbar">
